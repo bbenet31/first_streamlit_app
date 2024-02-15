a3b393345550a3913e4f9b9c80b89a426b5828f7
bbenet31 <159810881+bbenet31@users.noreply.github.com>
2024-02-15 09:28:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,11 +35,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import urllib3
-resp = urllib3.request("GET", "https://httpbin.org/robots.txt")
-resp.status
-200
-resp.data
-b"User-agent: *\nDisallow: /deny\n"
 
 import snowflake.connector
 
